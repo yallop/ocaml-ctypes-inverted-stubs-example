f23e7c8a2a8f09cc92e0fdc6f64bc384b6cd8f2a
Jeremy Yallop <yallop@gmail.com>
2015-11-10 15:19:42 +0000

    Add Travis support.

---
+++ .travis-ci.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-ocaml.sh
+sh .travis-ocaml.sh
+
+eval `opam config env`
+
+opam install --yes ctypes-foreign ctypes xmlm
+make
+make test

4dd0c0055fbc83ddfd2d5238ac3269fc69b0a905
Jeremy Yallop <yallop@gmail.com>
2018-05-23 19:07:03 +0100

    Add MIT licence

--- lib/apply_bindings.ml
+++ lib/apply_bindings.ml
@@ -1,3 +1,10 @@
+(*
+ * Copyright (c) 2014 Jeremy Yallop.
+ *
+ * This file is distributed under the terms of the MIT License.
+ * See the file LICENSE for details.
+ *)
+
 (** Apply the Stubs functor to the generated bindings to link the generated
     code into the library. *)
 include Bindings.Stubs(Xmlm_bindings)
